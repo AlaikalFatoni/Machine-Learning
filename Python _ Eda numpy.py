448deb48ea6144cf4638127d5d81d64c3680940a
Alex Alaikal Fatoni <m.alaikalfatoni@gmail.com>
2026-06-17 16:24:45 +0700

    Add files via upload

---
+++ Python _ Eda numpy.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#arr = np.array([[1, 2, 3, 4],
+                #[5, 6, 7, 8],
+                #[9, 10, 11, 12],
+                #[13, 14, 15, 16],
+                #[17, 18, 19, 20],
+                #[21, 22, 23, 24]])
+
+#s=np.sum(arr)
+#print(s)
+maximilian = np.arange(1,25)
+print(maximilian)
+theo = maximilian.reshape(6,4)
+print(theo)
+alexalaikal = np.sum(theo)
+print(alexalaikal)
+aurent = np.mean(theo)
+print(aurent)
+
